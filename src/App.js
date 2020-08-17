a909866c5cb06a034f7525fed5293cde6ef4de4e
Aditya Sharma <adityasharma.code@gmail.com>
2020-08-17 12:45:07 +0530

    sample image gallery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import imageJson from './images.json'
 import './App.css';
 
 function App() {
+  let [images, setImages] = useState([]);
+
+  useEffect(() => {
+    function getMeta(url) {
+        return new Promise((resolve, reject) => {
+            let img = new Image();
+            img.onload = () => resolve(img);
+            img.onerror = () => reject();
+            img.src = url;
+        });
+    }
+    Promise.all(imageJson.map(async (image) => await getMeta(image.url)))
+    .then(imageData => {
+        imageData.forEach(image => {
+          console.log(image)
+          console.log(image.naturalWidth, image.naturalHeight, image.src)
+        })
+        setImages(imageData.map(image => ({
+          src: image.src,
+          isPortrait: image.naturalWidth < image.naturalHeight
+        })))
+      }
+    )
+  }, [])
+  console.log(images)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        {
+          images.map((image) => <img src={image} />)
+        }
+      </div>
     </div>
   );
 }
